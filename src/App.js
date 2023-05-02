87e21e24347b6a1d8e60dd6c3507d9a178f979ab
sowmik-sec <sowmik.sec@gmail.com>
2023-05-02 11:05:26 +0600

    create protected route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./modules/Dashboard";
 import Form from "./modules/Form";
 
+const ProtectedRoute = ({ children }) => {
+  const isLoggedIn = localStorage.getItem("user:token") !== null;
+  if (!isLoggedIn && window.location.pathname === "/")
+    return <Navigate to={`/users/sign_in`} />;
+  else if (
+    isLoggedIn &&
+    ["/users/sign_in", "/users/sign_up"].includes(window.location.pathname)
+  ) {
+    return <Navigate to={"/"} />;
+  }
+  return children;
+};
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/users/sign_in" element={<Form isSignInPage={true} />} />
-      <Route path="/users/sign_up" element={<Form />} isSignInPage={false} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/users/sign_in"
+        element={
+          <ProtectedRoute>
+            <Form isSignInPage={true} />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/users/sign_up"
+        element={
+          <ProtectedRoute>
+            <Form isSignInPage={false} />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
     // <div className="bg-[#e2eafa] h-screen flex justify-center items-center">
     //   {/* <Form /> */}
